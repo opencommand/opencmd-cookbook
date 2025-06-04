13e99bb208d4db3adc7e2fa44fc4deff722cf87c
ansurfen <axf593161@gmail.com>
2025-06-04 19:52:35 +0800

    docs: add headers for go files

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// Copyright 2025 The OpenCmd Authors. All rights reserved.
+// Use of this source code is governed by a GPL-3.0
+// license that can be found in the LICENSE file.
 package main
 
 import (
